60ae3822525e39cbdedf0253f2b440090eae0727
NamDo8467 <huynhnamdo@gmail.com>
2021-10-15 15:22:49 +0700

    made /chat protected route

---
+++ client/src/ProtectedRoute.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { useAuth } from "./Auth/Auth";
+function ProtectedRoute({ component: Component, ...rest }) {
+  const { currentUser } = useAuth();
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        currentUser ? <Component {...props} /> : <Redirect to="/" />
+      }
+    />
+  );
+}
+
+export default ProtectedRoute;
