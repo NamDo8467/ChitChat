8d59726378249b5abac5c722a9eef5fe4873ff78
NamDo8467 <huynhnamdo@gmail.com>
2021-10-15 15:24:08 +0700

    add sign up with Google

--- client/src/Join/Join.jsx
+++ client/src/Join/Join.jsx
@@ -1,19 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useHistory } from "react-router-dom";
-import './Join.css'
+import { useAuth } from "../Auth/Auth";
+import "./Join.css";
 
 function Join({ location }) {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("Javascript");
+  const [password, setPassword] = useState("")
+  // const { currentUser, signInWithGoogle} = useContext(AuthContext)
+  const { currentUser, signInWithGoogle } = useAuth();
+
   let history = useHistory();
   const handleJoin = (e) => {
     if (!name || !room) {
-      
       e.preventDefault();
     } else {
       history.push(`/chat?name=${name}&room=${room}`);
     }
   };
+  useEffect(() => {
+    if (currentUser) {
+      history.push(`/chat?name=${currentUser}&room=${room}`);
+    }
+  }, [currentUser]);
+
   return (
     <form>
       <div className="greeting">
@@ -29,17 +39,34 @@ function Join({ location }) {
           required
         />
       </div>
+      <div className="passwordInput">
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Enter your password"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
       <div className="roomInput">
-        <select name="room" id="room" onChange={(e) => setRoom(e.target.value)}>
+        <select
+          name="room"
+          id="room"
+          defaultValue={"Javascript"}
+          onChange={(e) => setRoom(e.target.value)}
+        >
           <option value="Javascript">Javascript</option>
           <option value="Python">Python</option>
           <option value="Ruby">Ruby</option>
         </select>
       </div>
-      <a className='button' type='submit' onClick={handleJoin} >JOIN</a>
-      {/* <button type="submit" onClick={handleJoin}>
+      <a className="button" type="submit" onClick={handleJoin}>
         JOIN
-      </button> */}
+      </a>
+      <button type="submit" onClick={signInWithGoogle}>
+        Sign In with Google
+      </button>
     </form>
   );
 }
