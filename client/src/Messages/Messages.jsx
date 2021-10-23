429a310a86de536c9f03d74b359ba328c56b077f
NamDo8467 <huynhnamdo@gmail.com>
2021-10-23 10:08:32 +0700

    add usersInRoom to the props

--- client/src/Messages/Messages.jsx
+++ client/src/Messages/Messages.jsx
@@ -3,12 +3,13 @@ import Message from "./Message/Message";
 import ScrollToBottom from "react-scroll-to-bottom"
 import './Messages.css'
 
-function Messages({ messages, name }) {
+function Messages({ messages, name, users }) {
   return (
     <ScrollToBottom className="messagesContainer">
       {messages.map(message => {
+        // console.log(message.userUID);
         return (
-          <Message key={message.text} message={message.text} user={message.user} name={name} />
+          <Message key={message.text} message={message.text} user={message.user} name={name} usersInRoom={users} userUID={message.userUID} />
         );
       })}
     </ScrollToBottom>
