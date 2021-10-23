69fc2f514d7b11397685d6c36e4d736a85c4a116
NamDo8467 <huynhnamdo@gmail.com>
2021-10-23 10:10:53 +0700

    refactor the parameter names of all th the funtion

--- server/users.js
+++ server/users.js
@@ -1,23 +1,23 @@
 const users = [];
 
-const addUser = (id, name, room) => {
-  const user = { id, name, room };
+const addUser = (socketID, name, room, userUID) => {
+  const user = { socketID, name, room, userUID };
   users.push(user);
   return user;
 };
 
 const getUser = (name) => users.find((user) => user.name == name);
 
-const removeUser = (id) => {
-  const index = users.findIndex((user) => user.id == id);
+const removeUser = (socketID) => {
+  const index = users.findIndex((user) => user.socketID == socketID);
   if (index != -1) {
     return users.splice(index, 1)[0];
   }
 };
 
 const getUsersInRoom = (room) => {
-  const usersInRoom = users.filter(user => user.room == room)
-  return usersInRoom
-}
+  const usersInRoom = users.filter((user) => user.room == room);
+  return usersInRoom;
+};
 
 module.exports = { addUser, getUser, removeUser, getUsersInRoom };
